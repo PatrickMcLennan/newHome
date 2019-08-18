14043f1ad57a3c204d5ac0525406ee7a163339c6
Patrick <patrick.a.mclennan@gmail.com>
2019-08-18 14:47:24 -0400

    Front-end scaffolding done

--- client/src/es6.js
+++ client/src/es6.js
@@ -1,8 +1,31 @@
+const NOTIFICATIONS = {
+	facebook: ['one'],
+	gmail: ['one', 'two'],
+	work: ['one', 'two', 'three']
+};
+
 const DOM = {
+	navNotifications: [...document.querySelectorAll('.nav_notification_span')],
+	navLinks: [...document.querySelectorAll('.nav_button')],
+	main: document.querySelector('.main'),
 	googleButton: document.querySelector('.google_button'),
 	googleForm: document.querySelector('.google_form'),
 	googleInput: document.querySelector('.google_form_input')
 };
+
+const REQUESTS = {
+	getWeather: new XMLHttpRequest()
+};
+
+// NAV
+DOM.navLinks.forEach((link, i) =>
+	link.addEventListener('click', () => {
+		if (DOM.main.classList[DOM.main.classList.length - 1].toString().includes('main_viewport_offset')) {
+			DOM.main.classList.remove(DOM.main.classList[DOM.main.classList.length - 1]);
+		}
+		return DOM.main.classList.add(`main_viewport_offset_${i}`);
+	})
+);
 
 // GOOGLE FORM
 DOM.googleForm.addEventListener('submit', e => {
@@ -11,11 +34,23 @@ DOM.googleForm.addEventListener('submit', e => {
 	DOM.googleButton.click();
 });
 
+// ON PAGE LOAD, LET 'ER RIP
 (() => {
-	console.log('hello');
-	const getWeather = new XMLHttpRequest();
-	getWeather.addEventListener('load', console.log('loading'));
-	getWeather.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-	getWeather.send();
-	console.log([...getWeather.responseText]);
+	[...Object.values(REQUESTS)].forEach(request => {
+		request.addEventListener('load', console.log('loading'));
+		request.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+		request.send();
+		console.log([...REQUESTS.getWeather.responseText]);
+	});
+	return DOM.navNotifications.forEach(
+		spanElement =>
+			(spanElement.innerText =
+				NOTIFICATIONS[
+					`${
+						spanElement.classList[spanElement.classList.length - 1].split('_')[
+							spanElement.classList[spanElement.classList.length - 1].split('_').length - 1
+						]
+					}`
+				].length)
+	);
 })();
